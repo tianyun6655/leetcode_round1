1764f0105d7cf5db4a75660128ceaa4465991902
Tianyun <451617222@qq.com>
2018-09-10 16:19:32 -0700

    add: no 287

---
+++ array/no_287_09-10-2018.go
@@ -0,0 +1,27 @@
+package array
+
+/*
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+Note:
+
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+
+给定一个数组，找出其中的重复元素，假定给定的数组中只有1个重复的元素，
+要求：数组只读不可修改，空间复杂度为O(1)，时间复杂度必须低于 O(n^2)，只有1个重复的元素 但是可能重复多次
+*/
+
+func Tianyun_findDuplicate(nums []int) int {
+}
