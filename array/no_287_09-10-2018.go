655a3d6f36594415273d88a650a531cc510234d6
Tianyun <451617222@qq.com>
2018-09-10 16:19:52 -0700

    add: no 287

--- array/no_287_09-10-2018.go
+++ array/no_287_09-10-2018.go
@@ -14,10 +14,13 @@ Input: [3,1,3,4,2]
 Output: 3
 Note:
 
+
 You must not modify the array (assume the array is read only).
 You must use only constant, O(1) extra space.
 Your runtime complexity should be less than O(n2).
 There is only one duplicate number in the array, but it could be repeated more than once.
+
+link: https://leetcode.com/problems/find-the-duplicate-number/description/
 
 给定一个数组，找出其中的重复元素，假定给定的数组中只有1个重复的元素，
 要求：数组只读不可修改，空间复杂度为O(1)，时间复杂度必须低于 O(n^2)，只有1个重复的元素 但是可能重复多次
