3e096bc392d01e896ee74f8201d9f818a4021b16
Tianyun <451617222@qq.com>
2018-09-10 16:11:40 -0700

    add:subsets

--- array/no_78.go
+++ array/no_78.go
@@ -1,18 +1,28 @@
 package array
 
+import "fmt"
+
 /*
 Given a set of distinct integers, nums, return all possible subsets (the power set).
 
 Note: The solution set must not contain duplicate subsets.
 
-给定给一个整型数组，返回这个数组所有的子数组（不重复）
+给定给一个整型数组，返回这个数组所有的子集（不重复）
 
-Link: https://leetcode.com/problems/subsets/description/
+Link: https://leetcode.com/problems/subsets/descri,weption/
 
 */
 
-//
+//09-10-2018（PDT）,暂时有bug, 未解决
 func Tianyun_Subsets(nums []int) [][]int {
-
-
+	result:=make([][]int,0)
+	result=append(result,[]int{})
+	for i:=0;i<len(nums);i++{
+		length:=len(result)
+        for j:=0;j<length;j++{
+        	fmt.Println(i," :",result)
+          result = append(result,append(result[j],nums[i]))
+		}
+	}
+   return result
 }
--- array/no_78_test.go
+++ array/no_78_test.go
@@ -1,4 +1,12 @@
 package array
 
+import (
+	"testing"
+	"fmt"
+)
+
+func TestTianyun_Subsets(t *testing.T) {
+	fmt.Println(Tianyun_Subsets([]int{9,0,3,5,7}))
 
 
+}
